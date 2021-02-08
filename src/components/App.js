85ed85f9e4be8caf8e089667caaf41864ed8c5de
Vitalii Kovalchuk <vitaliikovalchuk55@gmail.com>
2021-02-09 00:05:58 +0200

    continue basic staff

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+
 import './App.css';
-import PhoneBook from './phoneBook/PhoneBook';
+// import PhoneBook from './phoneBook/PhoneBook';
 
 
-const App = () => {
-    return (
-        <PhoneBook />
-    );
+class App extends Component {
+    componentDidMount() {
+
+    }
+
+
+
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <Switch>
+
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
---
+++ src/views/ContactsView.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+class ContactsView extends Component {
+    state = {}
+    render() {
+        return (
+            <PhoneBook />
+        );
+    }
+}
+
+export default ContactsView;
